4cd5d6536638d2bfb73fe24d2f12b86d9a5e05ae
Noah Nguyen <nnoahnguyenn@gmail.com>
2024-05-22 17:30:26 -0400

    large change

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-const greetings = "Noah Nguyen\nType 'help' to see list of commands." 
+const font = '3D Diagonal';
 
-$(function() {
-    $('#terminal').terminal({
-        love: function(what) {
-            this.echo("To my love, " + what + ". Hi my love!, You are as cute as a button!");
-        },
-        // Define more commands here...
-    }, {
-        greetings
-    });
+figlet.defaults({ fontPath: 'https://unpkg.com/figlet/fonts/'});
+figlet.preloadFonts([font], ready);
+
+function render(text) {
+  const cols = term.cols();
+  return trim(figlet.textSync(text, {
+    font: font,
+    width: cols,
+    whitespaceBreak: true
+  }));
+}
+
+function trim(str) {
+  return str.replace(/[\n\s]+$/, '');
+}
+
+const commands = {
+  love: function(what) {
+      this.echo("To my love, " + what + ". Hi my love!, You are as cute as a button!");
+  },
+  // add more 
+};
+
+let term;
+
+function ready() {
+  term = $('#terminal').terminal(commands, {
+    greetings:false
+  });
+  term.pause();
+  term.echo(() => {
+    const ascii = render('Noah Nguyen');
+    return "${ascii}\nType 'help' for a list of available commands";
+  }).resume();
+}
+
+$(document).ready(function() {
+
 });
